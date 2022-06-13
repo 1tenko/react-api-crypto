c098d254e99a249cacb25f8f4f46ee0637d6fbdd
tenko <tajm.ogunbiyi@gmail.com>
2022-06-13 15:28:17 +0100

    :construction_worker: Coins API useEffect and useState setup + coins search started.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form action="">
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
     </div>
   );
 }
